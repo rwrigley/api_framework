82daaab50a1a107ad869221c7861e0bdc02d273c
ryan <ryan@servicefusion.com>
2019-08-06 18:51:35 -0500

    up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="api_framework",
-    version="0.0.4",
+    version="0.0.6",
     author="Ryan Wrigley",
     author_email="ryan@servicefusion.com    ",
     description="REST API framework based on peewee, marshmallow, falcon",
